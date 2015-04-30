50f4a4c8aa3192a5934704ddb602d6df8c3e69d9
unknown <kmuthusamy@QSIH-00256.Portal01.nextgen.com>
2015-04-30 14:54:05 +0530

    First One

---
+++ js/vel.js
@@ -0,0 +1,11 @@
+var vel=(function(){
+//All sweets here
+    function byId=function(id){
+        return document.getElementById(id);
+    }
+    
+    return {
+        byId:byId
+    }
+    
+})();
